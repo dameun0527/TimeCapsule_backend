1840f2c55a1809ceb7be547e134a2530dbe1fb29
dameun0527 <jisuryu0527@gmail.com>
2025-07-24 15:47:28 +0900

    feat(security): ValidPassword 어노테이션 추가

--- src/main/java/com/example/timecapsule_backend/controller/user/dto/valid/ValidPassword.java
+++ src/main/java/com/example/timecapsule_backend/controller/user/dto/valid/ValidPassword.java
@@ -1,4 +1,19 @@
 package com.example.timecapsule_backend.controller.user.dto.valid;
 
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@Constraint(validatedBy = PasswordValidator.class)
 public @interface ValidPassword {
+
+    String message() default "영문자 숫자 조합 8 ~ 12자 사이로 입력해야 합니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
