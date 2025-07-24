e64147d7d9534920d4525f2be00bca673354502e
dameun0527 <jisuryu0527@gmail.com>
2025-07-24 15:24:54 +0900

    feat(security): 보안 응답 핸들러 구현 - 인증/인가 성공, 실패 포맷 통일

--- src/main/java/com/example/timecapsule_backend/config/security/handler/SecurityResponseHandler.java
+++ src/main/java/com/example/timecapsule_backend/config/security/handler/SecurityResponseHandler.java
@@ -1,4 +1,43 @@
 package com.example.timecapsule_backend.config.security.handler;
 
+import com.example.timecapsule_backend.util.api.ApiError;
+import com.example.timecapsule_backend.util.api.ApiResult;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+
+@Slf4j
+@RequiredArgsConstructor
 public class SecurityResponseHandler {
+
+    private final ObjectMapper objectMapper;
+
+    public void fail(HttpServletResponse response, String msg, HttpStatus httpStatus) {
+        try {
+            ApiResult<ApiError> error = ApiResult.error(httpStatus.value(), msg);
+            String responseBody = objectMapper.writeValueAsString(error);
+
+            response.setContentType("application/json; charset=utf-8");
+            response.setStatus(httpStatus.value());
+            response.getWriter().write(responseBody);
+            response.getWriter().flush();
+        } catch (Exception e) {
+            log.error("시큐리티 예외 응답 생성 중 오류 발생: {}", e.getMessage(), e);
+        }
+    }
+
+
+    public void success(HttpServletResponse response, Object object) {
+        try {
+            String responseBody = objectMapper.writeValueAsString(ApiResult.success(object));
+            response.setContentType("application/json; charset=utf-8");
+            response.setStatus(HttpStatus.OK.value());
+            response.getWriter().write(responseBody);
+            response.getWriter().flush();
+        } catch (Exception e) {
+            log.error("시큐리티 성공 응답 생성 중 오류 발생: {}", e.getMessage(), e);
+        }
+    }
 }
