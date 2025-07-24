a06a9d1c4c76d4c10f3f64347f0988133a2a7f20
dameun0527 <jisuryu0527@gmail.com>
2025-07-24 15:46:08 +0900

    feat(security): 비밀번호 유효성 검증

--- src/main/java/com/example/timecapsule_backend/controller/user/dto/valid/PasswordValidator.java
+++ src/main/java/com/example/timecapsule_backend/controller/user/dto/valid/PasswordValidator.java
@@ -1,4 +1,20 @@
 package com.example.timecapsule_backend.controller.user.dto.valid;
 
-public class PasswordValidator {
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PasswordValidator implements ConstraintValidator<ValidPassword, String> {
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext context) {
+        if (password == null) {
+            return true;
+        }
+
+        if (password.length() < 8 || password.length() > 12) {
+            return false;
+        }
+
+        return password.chars().allMatch(Character::isLetterOrDigit);
+    }
 }
