6cb96bc245564bdd06000c429aec9c4307fc4e88
dameun0527 <jisuryu0527@gmail.com>
2025-07-24 15:47:43 +0900

    feat(signup): 회원가입 로직 추가

---
+++ src/main/java/com/example/timecapsule_backend/service/UserService.java
@@ -0,0 +1,48 @@
+package com.example.timecapsule_backend.service;
+
+import com.example.timecapsule_backend.config.security.loginUser.dto.LoginRequest;
+import com.example.timecapsule_backend.config.security.loginUser.dto.LoginResponse;
+import com.example.timecapsule_backend.controller.user.dto.request.SignupRequestDto;
+import com.example.timecapsule_backend.domain.user.Role;
+import com.example.timecapsule_backend.domain.user.User;
+import com.example.timecapsule_backend.domain.user.UserRepository;
+import com.example.timecapsule_backend.ex.BusinessException;
+import com.example.timecapsule_backend.ex.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    /**
+     * 회원 가입
+     * @param requestDto 가입 요청 정보
+     * @return 가입 결과
+     */
+    @Transactional
+    public String signup(SignupRequestDto requestDto) {
+        String name = requestDto.getName();
+        String password = passwordEncoder.encode(requestDto.getPassword());
+        String email = requestDto.getEmail();
+
+        // 이메일 중복 확인
+        if (userRepository.existsByEmail(email)) {
+            throw new BusinessException(ErrorCode.USER_EMAIL_ALREADY_EXISTS);
+        }
+
+        Role role = Role.USER;
+        User user = new User(name, password, email, role);
+        userRepository.save(user);
+        return "회원 가입 성공";
+    }
+
+}
