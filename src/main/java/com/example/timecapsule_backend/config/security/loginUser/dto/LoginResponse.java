b7d28fcc71f819352bd6e2dd6994215452054acb
dameun0527 <jisuryu0527@gmail.com>
2025-07-24 15:26:25 +0900

    feat(login): 로그인 응답 dto 추가 - 사용자 정보 반환 구조

--- src/main/java/com/example/timecapsule_backend/config/security/loginUser/dto/LoginResponse.java
+++ src/main/java/com/example/timecapsule_backend/config/security/loginUser/dto/LoginResponse.java
@@ -1,4 +1,19 @@
 package com.example.timecapsule_backend.config.security.loginUser.dto;
 
+import com.example.timecapsule_backend.domain.user.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class LoginResponse {
+
+    public static final String SUCCESS = "로그인 성공";
+    private Long id;
+    private String message;
+
+    public LoginResponse(User user) {
+        this.id = user.getId();
+        this.message = SUCCESS;
+    }
 }
