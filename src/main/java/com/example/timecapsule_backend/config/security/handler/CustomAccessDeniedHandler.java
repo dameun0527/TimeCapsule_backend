f0130e28916d6fa3d9db3b86b54e672b5cda4f93
dameun0527 <jisuryu0527@gmail.com>
2025-07-24 15:21:41 +0900

    feat(security): 접근 제한 핸들러 구현 - 403

--- src/main/java/com/example/timecapsule_backend/config/security/handler/CustomAccessDeniedHandler.java
+++ src/main/java/com/example/timecapsule_backend/config/security/handler/CustomAccessDeniedHandler.java
@@ -1,4 +1,21 @@
 package com.example.timecapsule_backend.config.security.handler;
 
-public class CustomAccessDeniedHandler {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+@Slf4j
+@RequiredArgsConstructor
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final SecurityResponseHandler securityResponseHandler;
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException e) {
+        log.error("권한 없음: {}", e.getMessage(), e);
+        securityResponseHandler.fail(response, "권한이 없습니다.", HttpStatus.FORBIDDEN);
+    }
 }
