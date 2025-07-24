5b39b3b00a05090d2732ea9a4f6a6b42665f3b3f
dameun0527 <jisuryu0527@gmail.com>
2025-07-24 15:18:55 +0900

    feat(jwt): JWT 상수 인터페이스 - 헤더명, 토큰 접두사, 만료시간 상수 정의

--- src/main/java/com/example/timecapsule_backend/config/jwt/JwtVo.java
+++ src/main/java/com/example/timecapsule_backend/config/jwt/JwtVo.java
@@ -1,4 +1,8 @@
 package com.example.timecapsule_backend.config.jwt;
 
 public interface JwtVo {
+
+    public String HEADER = "Authorization";
+    public String TOKEN_PREFIX = "Bearer ";
+    public Long EXPIRED_TIME = 1000 * 60 * 60 * 24 * 7L;
 }
