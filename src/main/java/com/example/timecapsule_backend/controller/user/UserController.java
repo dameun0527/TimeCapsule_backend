59682e58f6bc635b881558007c3f8ff90d3d6c91
dameun0527 <jisuryu0527@gmail.com>
2025-07-24 15:47:50 +0900

    feat(signup): 회원가입 api 추가

--- src/main/java/com/example/timecapsule_backend/controller/user/UserController.java
+++ src/main/java/com/example/timecapsule_backend/controller/user/UserController.java
@@ -1,4 +1,25 @@
 package com.example.timecapsule_backend.controller.user;
 
+
+import com.example.timecapsule_backend.controller.user.dto.request.SignupRequestDto;
+import com.example.timecapsule_backend.service.UserService;
+import com.example.timecapsule_backend.util.api.ApiResult;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/auth/register")
+    public ResponseEntity<ApiResult> signup(@Valid @RequestBody SignupRequestDto requestDto) {
+        String message = userService.signup(requestDto);
+        return ResponseEntity.ok(ApiResult.success(message));
+    }
+
 }
