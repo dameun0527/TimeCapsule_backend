64378ee90b1e6f25860226748d8e18d9c0b6ecb3
dameun0527 <jisuryu0527@gmail.com>
2025-07-24 15:27:19 +0900

    feat(security): 사용자 조회 서비스(UserDetailsService 구현체)

--- src/main/java/com/example/timecapsule_backend/config/security/loginUser/LoginUserService.java
+++ src/main/java/com/example/timecapsule_backend/config/security/loginUser/LoginUserService.java
@@ -1,4 +1,23 @@
 package com.example.timecapsule_backend.config.security.loginUser;
 
-public class LoginUserService {
+import com.example.timecapsule_backend.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class LoginUserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(LoginUser::new)
+                .orElseThrow(() -> new InternalAuthenticationServiceException("로그인 실패"));
+    }
 }
