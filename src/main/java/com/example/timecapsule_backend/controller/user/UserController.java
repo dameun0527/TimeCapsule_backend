0a9a1d48c21527bb36d21ea1c5b733126d0df50f
dameun0527 <jisuryu0527@gmail.com>
2025-07-25 16:14:43 +0900

    feat(user): 사용자 전용 페이지 조회, 수정 api 추가

--- src/main/java/com/example/timecapsule_backend/controller/user/UserController.java
+++ src/main/java/com/example/timecapsule_backend/controller/user/UserController.java
@@ -1,16 +1,18 @@
 package com.example.timecapsule_backend.controller.user;
 
 
+import com.example.timecapsule_backend.config.security.loginUser.LoginUser;
 import com.example.timecapsule_backend.controller.user.dto.request.SignupRequestDto;
+import com.example.timecapsule_backend.controller.user.dto.request.UserUpdateRequest;
+import com.example.timecapsule_backend.controller.user.dto.response.UserResponse;
+import com.example.timecapsule_backend.controller.user.dto.response.UserUpdateResponse;
 import com.example.timecapsule_backend.service.UserService;
 import com.example.timecapsule_backend.util.api.ApiResult;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,4 +27,19 @@ public class UserController {
         return ResponseEntity.ok(ApiResult.success(message));
     }
 
+    @GetMapping("/users/mypage")
+    public ResponseEntity<ApiResult<UserResponse>> getMyPage(@AuthenticationPrincipal LoginUser loginUser) {
+        Long userId = loginUser.getUser().getId();
+        return ResponseEntity.ok(ApiResult.success(userService.getMyPage(userId)));
+    }
+
+    @PatchMapping("/users/mypage")
+    public ResponseEntity<ApiResult<UserUpdateResponse>> updateMyPage(
+            @AuthenticationPrincipal LoginUser loginUser,
+            @Valid @RequestBody UserUpdateRequest userUpdateRequest
+            ) {
+        Long userId = loginUser.getUser().getId();
+        UserUpdateResponse response = userService.updateMyPage(userId, userUpdateRequest);
+        return ResponseEntity.ok(ApiResult.success(response));
+    }
 }
